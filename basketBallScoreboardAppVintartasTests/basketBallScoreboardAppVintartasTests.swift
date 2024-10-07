dc173796af716ccc057cfdcecfc89452ceb5f160
NV <nv2009123@gmail.com>
2024-10-07 17:57:29 -0500

    Initial Commit

---
+++ basketBallScoreboardAppVintartasTests/basketBallScoreboardAppVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  basketBallScoreboardAppVintartasTests.swift
+//  basketBallScoreboardAppVintartasTests
+//
+//  Created by Nicholas Vintartas on 10/7/24.
+//
+
+import Testing
+@testable import basketBallScoreboardAppVintartas
+
+struct basketBallScoreboardAppVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
